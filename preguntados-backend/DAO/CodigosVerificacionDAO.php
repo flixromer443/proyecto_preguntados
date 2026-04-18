4a675fbadbc6f501b263e19a14ed4ceb0411ba77
flixromer443 <flixromer443@gmail.com>
2026-04-18 13:21:15 -0300

    corrijo bug en la rutina de validacion de codigo verificador

--- preguntados-backend/DAO/CodigosVerificacionDAO.php
+++ preguntados-backend/DAO/CodigosVerificacionDAO.php
@@ -43,53 +43,36 @@ class CodigosVerificacionDAO {
 
     public function existeCodigoDeVerificacion($idUsuario, $codigo) : bool {
         try {
+            $elCodigoDeVerificacionEsValido = false;
             $this->pdo->beginTransaction();
     
-            $stmt = $this->pdo->prepare("SELECT id, intentos_consulta 
-                                         FROM codigos_verificacion 
+            $stmt = $this->pdo->prepare("SELECT * FROM codigos_verificacion 
                                          WHERE id_usuario = :id_usuario
-                                           AND codigo = :codigo
-                                         FOR UPDATE
                                         ");
     
-            $stmt->execute([
-                ':id_usuario' => $idUsuario,
-                ':codigo' => $codigo
-            ]);
-    
+            $stmt->execute([':id_usuario' => $idUsuario]);
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
     
-            if (!$row) {
+            if($row){
+                $elCodigoDeVerificacionEsValido = $row['codigo'] == $codigo;
+
+                $intentos = (int)$row['intentos_consulta'] + 1;
+                $stmtUpdate = $this->pdo->prepare("UPDATE codigos_verificacion
+                                                   SET intentos_consulta = :intentos
+                                                   WHERE id = :id
+                                                    ");
+                $stmtUpdate->execute([':intentos' => $intentos,':id' => $row['id'] ]);
+
+                if ($intentos >= 3 || $elCodigoDeVerificacionEsValido) {
+                    $stmtDelete = $this->pdo->prepare("DELETE FROM codigos_verificacion
+                                                       WHERE id = :id
+                                                      ");
+                    $stmtDelete->execute([':id' => $row['id']]);
+                }
                 $this->pdo->commit();
-                return false;
+
             }
-    
-            $intentos = (int)$row['intentos_consulta'] + 1;
-    
-            $stmtUpdate = $this->pdo->prepare("UPDATE codigos_verificacion
-                                               SET intentos_consulta = :intentos
-                                               WHERE id = :id
-                                            ");
-    
-            $stmtUpdate->execute([
-                ':intentos' => $intentos,
-                ':id' => $row['id']
-            ]);
-    
-            if ($intentos >= 3 || $row) {
-                $stmtDelete = $this->pdo->prepare("
-                    DELETE FROM codigos_verificacion
-                    WHERE id = :id
-                ");
-    
-                $stmtDelete->execute([
-                    ':id' => $row['id']
-                ]);
-            }
-    
-            $this->pdo->commit();
-    
-            return true;
+            return $elCodigoDeVerificacionEsValido;
     
         } catch (PDOException $e) {
             $this->pdo->rollBack();
